1c66413bbc2c0a8327d808c16c0fe5c0f53af7c5
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2024-06-09 12:24:07 +0100

    Use body parsing middleware

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
+// Middleware
 app.use(cors());
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+// Routers
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
