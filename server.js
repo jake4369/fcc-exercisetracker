4cc3c59474a48616a3a59f0840c9d076a9e20f64
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2024-06-09 12:16:55 +0100

    Connect to db

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
-const dotenv = require("dotenv").config();
-const app = require("./index");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const colors = require("colors");
 
-console.log(process.env.PORT);
+dotenv.config();
+
+const app = require("./index");
+const DB = process.env.MONGODB_URI.replace(
+  "<PASSWORD>",
+  process.env.MONGODB_PASSWORD
+);
+
+mongoose
+  .connect(DB)
+  .then(() =>
+    console.log(colors.magenta.underline("\nDatabase succesfully connected"))
+  )
+  .catch((err) => console.error(err));
 
 // Server
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log("Your app is listening on port " + listener.address().port);
+  console.log(
+    colors.blue.underline(
+      "\nYour app is listening on port " + listener.address().port
+    )
+  );
 });
