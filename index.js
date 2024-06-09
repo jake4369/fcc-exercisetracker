3397d0feaf2d9a8b7c5043158858abf20a8bee6e
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2024-06-09 12:28:54 +0100

    Import user router

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const userRouter = require("./routes/userRoutes");
 
 // Middleware
 app.use(cors());
@@ -11,5 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
+
+app.use("/api/users", userRouter);
 
 module.exports = app;
