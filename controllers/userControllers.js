5fa38ef1869f4712b1fa54fffb98bc0798c16699
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2024-06-09 13:35:56 +0100

    POST - createExercise

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -11,9 +11,11 @@ exports.createUser = async (req, res) => {
       });
     }
 
-    const newUser = await User.create({
+    let newUser = await User.create({
       username: req.body.username,
     });
+
+    newUser = await User.findById(newUser._id).select("-exercises -logs");
 
     res.status(201).json(newUser);
   } catch (error) {
@@ -32,6 +34,54 @@ exports.getAllUsers = async (req, res) => {
     const users = await User.find();
 
     res.status(200).json(users);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error: error.message,
+    });
+  }
+};
+
+// @Desc - Create new exercise
+// @Method - POST
+// @Route - /api/users/:_id/exercises
+exports.createExercise = async (req, res) => {
+  try {
+    const id = req.params._id;
+    const user = await User.findById(id);
+
+    if (!user) {
+      return res.status(404).json({
+        message: "No user found",
+      });
+    }
+
+    const username = user.username;
+    const description = req.body.description;
+    const duration = parseInt(req.body.duration);
+    const date = req.body.date
+      ? new Date(req.body.date).toDateString()
+      : new Date().toDateString();
+
+    if (!description || !duration) {
+      return res.status(400).json({
+        message: "Please fill in all required fields",
+      });
+    }
+
+    const newExercise = {
+      username,
+      id,
+      description,
+      duration,
+      date,
+    };
+
+    user.exercises.push(newExercise);
+
+    await user.save();
+
+    res.status(201).json(newExercise);
   } catch (error) {
     console.log(error);
     res.status(500).json({
